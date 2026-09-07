e433144767fdcd6be7426a2c4663634a62b45eb1
gustavoeugenio <gustavogeografia@gmail.com>
2026-09-07 01:23:05 -0300

    adicionado servidor local

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'projeto_sabiaviagens'))
 
 # Aponta para o arquivo wsgi do seu app Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'projeto_sabiaviagens.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'projeto_sabiaviagens.projeto_sabiaviagens.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
